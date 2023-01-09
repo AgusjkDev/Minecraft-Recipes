4216e53d201e3d677b34797543da81a124f5b8b2
Agustín Arnoldi <agusjkdev7@gmail.com>
2023-01-09 15:19:25 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,13 +4,13 @@ const Footer = () => {
     return (
         <footer className="flex w-full items-center justify-center bg-neutral-200 py-3">
             <a
-                href="https://github.com/ShadeDev7"
+                href="https://github.com/AgusjkDev"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-center justify-center gap-1"
             >
                 <div className="relative h-7 w-7">
-                    <Image alt="ShadeDev7 GitHub" src="/imgs/github.svg" layout="fill" />
+                    <Image alt="AgusjkDev GitHub" src="/imgs/github.svg" layout="fill" />
                 </div>
 
                 <p className="font-medium">ShadeDev7</p>
